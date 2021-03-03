7acd8a25a2b1f3408e0efa3fb5b1377bf6e54c4f
Sylvain Tran <sylvain.l.tran@gmail.com>
2021-03-02 19:21:19 -0500

    Added sandboxing assets, bezier path editor

---
+++ Assets/ShopStall.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopStall : MonoBehaviour
+{
+    // private int itemCapacity;
+    // private TraderAI currentOwner;
+    // private Item[] itemsDisplayed;
+
+    // public void SetupShopStall(int itemCapacity, TraderAI currentOwner, Item[] itemsDisplayed)
+    // {
+    //     this.itemCapacity = itemCapacity;
+    //     this.currentOwner = currentOwner;
+    //     this.itemsDisplayed = itemsDisplayed;
+    // }
+    // /**
+    // * Returns the info of 
+    // * this shop stall.
+    // */
+    // public override String ToString()
+    // {
+    //     return $"Item Capacity: {itemCapacity}\n" +
+    //            $"Current Owner: {currentOwner}\n" +
+    //            $"Items Displayed: {itemsDisplayed}";                                                            
+    // }
+}
