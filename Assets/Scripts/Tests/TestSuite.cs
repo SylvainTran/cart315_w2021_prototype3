8939972befa80ba8d5ca81d9377c9750a34495e0
SylvainTran <45033804+SylvainTran@users.noreply.github.com>
2021-03-04 19:01:59 -0500

    Added csv for trivia data, and file reader/parser to get it in-game
    Note to self: use Trim to clean up data, and the debugger to save hours.

--- Assets/Scripts/Tests/TestSuite.cs
+++ Assets/Scripts/Tests/TestSuite.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
 
 public class TestSuite : MonoBehaviour
 {
     // Trivia quiz mini-game tests
+    string[] db = { "Id,Answer,Question,Choices", "1,C, 'How many twinkies did you eat on the morning of September 11th, 2001?',A:20;B:3;C:12" };
     [SetUp]
     public void Setup()
     {
-
+        Debug.Log(db[1]);
     }
     [UnityTest]
     public IEnumerator TriviaCanvasExists()
@@ -18,5 +22,74 @@ public class TestSuite : MonoBehaviour
         triviaCanvas.SetActive(true);
         yield return new WaitForSeconds(0.1f);
         Assert.True(triviaCanvas.gameObject.activeSelf);
+    }
+    [UnityTest]
+    public IEnumerator TestReadCSVDbCorrectly()
+    {
+        yield return new WaitForSeconds(0.1f);
+        ReadCSVDbCorrectly();
+    }
+    public string[] ReadCSVDbCorrectly()
+    {
+        // Get the file
+        string pathToDb = $"{Application.dataPath}/triviadb.csv";
+        string db;
+        string[] csvLineDelimiter = { ",,," };
+        string[] lines;
+        try
+        {
+            // Open the text file using a stream reader and the ",,," csv delimiter
+            using (var sr = new StreamReader($"{pathToDb}"))
+            {
+               db = sr.ReadToEnd();
+               lines = db.Split(csvLineDelimiter, System.StringSplitOptions.RemoveEmptyEntries);
+               foreach (string line in lines)
+               {
+                   Debug.Log(line);
+                   Assert.IsNotNull(line);
+               }
+               return lines;
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            Debug.LogError(e.Message);
+        }
+        return null;
+    }
+    [UnityTest]
+    public IEnumerator TestParseDb()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Dictionary<string, string[]> triviaDbTableTest = new Dictionary<string, string[]>();
+        Dictionary<string, string[]> t = ParseTriviaDatabase(triviaDbTableTest, ReadCSVDbCorrectly());
+        string[] _t;
+        t.TryGetValue("1", out _t);
+        if(_t.Length > 0)
+        {
+            string testLine = _t[2];
+            Debug.Log(testLine);
+            StringAssert.AreEqualIgnoringCase("How many twinkies did you eat on the morning of September 11th, 2001?", testLine);
+        } else
+        {
+            Debug.Log("T is not what you think it is");
+        }
+        
+    }
+
+    public Dictionary<string, string[]> ParseTriviaDatabase(Dictionary<string, string[]> table, string[] triviaDb)
+    {
+        int i = triviaDb[0].Equals("Id,Answer,Question,Choices") ? 1 : 0; // first row is the header in a csv file
+        for (; i < triviaDb.Length; i++)
+        {
+            string[] fields = triviaDb[i].Split(',');
+            string choices = fields[3];
+            string question = fields[2];
+            string answer = fields[1];
+            string id = fields[0];
+            Debug.Log($"{id}");
+            table.Add(id, fields);
+        }
+        return table;
     }
 }
--- Assets/Scripts/Trivia/TriviaController.cs
+++ Assets/Scripts/Trivia/TriviaController.cs
@@ -1,25 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
 public class TriviaController : MonoBehaviour
 {
     public static GameObject triviaCanvas;
-    public GameObject choiceButton;
-    public GameObject choiceLabel;
+    public static GameObject choiceButton;
+    public static GameObject choiceLabel;
+    private static string[] triviaDb;
+    private static string[] questions;
+    private static string[] _choices;
+    private static string[] answers;
 
     private void Start()
     {
         triviaCanvas = Instantiate(Resources.Load<GameObject>("UI/TriviaCanvas"));
-        triviaCanvas.SetActive(true);
-        DontDestroyOnLoad(this);
+        triviaCanvas.SetActive(false);
+        LoadTriviaDatabase("triviadb.csv");
+        SetupTriviaQuestionsAndChoices();
+        ParseTriviaDatabase();
+        LogInfo();
+        //choiceButton = Instantiate(Resources.Load<GameObject>("ChoicePrefab"));
+        //choiceLabel = Instantiate(Resources.Load<GameObject>("ChoiceLabel"));
     }
+
+    private static void LogInfo()
+    {
+        for (int i = 0; i < questions.Length; i++)
+        {
+            Debug.Log($"{questions[i]}");
+            Debug.Log($"{_choices[i]}");
+            Debug.Log($"{answers[i]}");
+        }
+    }
+
+    private static void SetupTriviaQuestionsAndChoices()
+    {
+        questions = new string[triviaDb.Length - 1];
+        _choices = new string[triviaDb.Length - 1];
+        answers = new string[triviaDb.Length - 1];
+    }
+
     /**
-     * To begin the trivia mini-game,
-     * we need to show the trivia panel.
-     */
-    public static void ShowTriviaPanel(bool enabled)
+    * To begin the trivia mini-game,
+    * we need to show the trivia panel.
+    */
+    public static void ShowUIPanel(bool enabled)
     {
         triviaCanvas.SetActive(enabled);
+    }
+    /**
+     * To read the trivia questions, choices and answer,
+     * we need to load the db from a file (csv for ease of design).
+     * @args dbName : string name of file     
+     */
+    public static void LoadTriviaDatabase(string dbName)
+    {
+        // Get the file
+        string pathToDb = $"{Application.dataPath}/{dbName}";
+        try
+        {
+            // Open the text file using a stream reader and the ",,," csv delimiter
+            using (var sr = new StreamReader($"{pathToDb}"))
+            {
+                string db = sr.ReadToEnd();
+                string[] csvLineDelimiter = { ",,," };
+                triviaDb = db.Split(csvLineDelimiter, System.StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            Debug.LogError(e.Message);
+        }
+    }
+    /**
+     * To get the fields, 
+     * we need to parse the string db
+     * and put it into a dictionary for ease.
+     * @args table : dictionary hash table
+     * @args triviaDb : string[] the db
+     */
+    public static void ParseTriviaDatabase()
+    {
+        int i = triviaDb[0].Equals("Id,Answer,Question,Choices") ? 1 : 0; // first row is the header in a csv file
+        for(; i < triviaDb.Length; i++)
+        {
+            string[] fields = triviaDb[i].Split(',');
+            string id = fields[0].Trim(new char[] { '\r', '\n' });
+            string choices = fields[3].Trim(new char[] { '\r', '\n' });
+            string question = fields[2].Trim(new char[] { '\r', '\n' });
+            string answer = fields[1].Trim(new char[] { '\r', '\n' });
+            questions[i-1] = question;
+            _choices[i-1] = choices;
+            answers[i-1] = answer;
+        }
     }
 }
